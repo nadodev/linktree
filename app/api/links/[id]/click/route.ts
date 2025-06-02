1bd51f18cb9f7d27516552e823bfa7052dc3bab9
User <bemmenos@gmail.com>
2025-06-02 18:46:31 -0300

    feat: finaliza dashboard

--- app/api/links/[id]/click/route.ts
+++ app/api/links/[id]/click/route.ts
@@ -1,30 +1,18 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/app/lib/prisma';
 
-export async function POST(
-  request: Request,
-  context: {
-    params: {
-      id: string;
-    };
-  }
-) {
+export const POST = async (request: Request, context: any) => {
   try {
+    const { id } = context.params;
     const link = await prisma.link.update({
-      where: { id: context.params.id },
-      data: {
-        clicks: {
-          increment: 1,
-        },
-      },
+      where: { id },
+      data: { clicks: { increment: 1 } },
     });
-
     return NextResponse.json({ clicks: link.clicks });
   } catch (error) {
-    console.error('Error updating click count:', error);
     return NextResponse.json(
       { message: 'Internal server error' },
       { status: 500 }
     );
   }
-}
+};
